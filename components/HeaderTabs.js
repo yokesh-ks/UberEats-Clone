65bc656c268b6743116917103e6ff7a39a783104
yokesh-ks <ksyokesh98@gmail.com>
2021-12-25 21:27:46 +0530

    UberEats apps initial files

---
+++ components/HeaderTabs.js
@@ -0,0 +1,26 @@
+import React, {useState} from 'react'
+import { View, Text, TouchableOpacity } from 'react-native'
+
+export default function HeaderTabs() {
+    const [activeTab, setActiveTab] = useState("Delivery");
+    return (
+        <View style={{flexDirection: "row", alignSelf: "center"}}>
+            <HeaderButton text="Delivery" btncolor="black" textcolor="white" activeTab = {activeTab} setActiveTab = {setActiveTab}/>
+            <HeaderButton text="Pickup" btncolor="white" textcolor="black" activeTab = {activeTab} setActiveTab = {setActiveTab}/>
+        </View>
+    )
+}
+
+const HeaderButton = (props) => (
+    <TouchableOpacity 
+        style={{
+            backgroundColor: props.activeTab === props.text ? "black" : "white",
+            paddingVertical: 6,
+            paddingHorizontal: 16,
+            borderRadius: 30,
+        }}
+        onPress = {() => props.setActiveTab(props.text)}
+    >
+        <Text style={{ color: props.activeTab === props.text ? "white" : "black", fontSize: 15 }}>{props.text}</Text>
+    </TouchableOpacity>
+)
